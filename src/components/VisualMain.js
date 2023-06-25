1ef54ea4b83e0079ca6ccb778e39c2b1a97d0cb6
SeoHwan Yun <ctr_nike@naver.com>
2023-06-26 00:46:17 +0900

    20230626 스와이퍼 슬라이더 롤백

--- src/components/VisualMain.js
+++ src/components/VisualMain.js
@@ -13,6 +13,26 @@ import 'swiper/css/pagination';
 import './visual_main.scss';
 
 export default function VisualMain() {
+  const slides = [
+    {
+      id: 'vm01',
+      src: process.env.PUBLIC_URL + `images/visual_main01.jpg`,
+    },
+    {
+      id: 'vm02',
+      src: process.env.PUBLIC_URL + `images/visual_main02.jpg`,
+    },
+    {
+      id: 'vm03',
+      src: process.env.PUBLIC_URL + `images/visual_main03.jpg`,
+    },
+    {
+      id: 'vm04',
+      src: process.env.PUBLIC_URL + `images/visual_main04.jpg`,
+    },
+
+  ]
+
   const pageMent = [
     '2023 봄 여름 캠페인 스웨터 셔츠',
     '2023 봄 자켓',
@@ -22,18 +42,21 @@ export default function VisualMain() {
 
   const navigate = useNavigate();
 
-  const [swiperState, setSwiper] = useState([]);
-  const swiper = useRef([]);
-  
-  const slides = Array.from({ length: 4 }).map(
-    (el, index) => `Slide ${index + 1}`
-  );
+  const [vmIndex, setVmIndex] = useState(0);
 
-  const swiperProps = {
-    className: "visual_main_wrapper",
-    modules: [Virtual, Navigation, Pagination],
+  useEffect(() => {
+    console.log(vmIndex)
+  },[vmIndex]);
+
+/*   function indexChange(value) {
+    setVmIndex(value);
+  } */
+
+  const swiperWrapper = {
+    className: `visual_main_wrapper`,
+    modules: [Navigation, Pagination],
+    spaceBetween: 0,
     loop: true ,
-    speed: 800, 
     slidesPerView: 1, 
     navigation: true, 
     //pagination은 현재 슬라이더 갯수만큼 생김
@@ -41,27 +64,25 @@ export default function VisualMain() {
 
     //isActive active클래스가 확인기능
     pagination: {
-      renderBullet: function(index, className='') {
-        return `<span class="page_indicator">${pageMent[index]}</span>`
+      clickable: true,
+      renderBullet: function(index, className) {
+        return `<span class=${className}>${pageMent[index]}</span>`
       }
     },
-    virtual: true, 
-    ref: swiper,
   }
 
-  useEffect(() => {
-    console.log({...swiperProps});
-  },[]);
-
+  const swiper = {
+    className: 'visual_main',
+    onClick: () => {navigate('/top')},
+  }
 
   //스프레드 문법으로 객체를 prop에다 넣으면 해당 객체의 속성들이
   //개별로 전달되게 된다.
   return (
-
-    <Swiper {...swiperProps}>
-      {slides.map((slideContent, index) => (
-        <SwiperSlide className="visual_main" key={slideContent} virtualIndex={index} onClick={() => {navigate('/top')}}>
-          <img src = {process.env.PUBLIC_URL + `images/visual_main0${index + 1}.jpg`} />
+    <Swiper {...swiperWrapper}>
+      {slides.map((object) => (
+        <SwiperSlide {...swiper} key={object.id}>
+          <img src = {object.src}/>
         </SwiperSlide> 
       ))}
     </Swiper>
